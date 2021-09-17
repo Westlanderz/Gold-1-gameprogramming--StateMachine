df4345e03633f9374ef0e0bbc0535831e3c40bc6
Noah Knegt <github@noahknegt.com>
2021-09-17 19:43:52 +0200

    Working state machine
    Signed-off-by: Noah Knegt <github@noahknegt.com>

--- Assets/Scripts/CubeStateMachine.cs
+++ Assets/Scripts/CubeStateMachine.cs
@@ -4,23 +4,81 @@ using UnityEngine;
 
 public class CubeStateMachine : MonoBehaviour
 {
-    [SerializeField]private CubeState currentState;
-    [SerializeField] float speed;
+    [SerializeField] private CubeState currentState;
     
     Rigidbody rb;
-    void Start() {
+    [SerializeField] private float moveSpeed = 2f;            // Player's speed when walking.
+    [SerializeField] private float rotationSpeed = 2f;
+    [SerializeField] private float jumpHeight = 5f;         // How high Player jumps
+
+    Vector3 moveDirection;
+
+    // Using the Awake function to set the references
+    void Awake()
+    {
         rb = GetComponent<Rigidbody>();
     }
-    
-    void Update() {
-        float x = Input.GetAxisRaw("Horizontal");
-        float z = Input.GetAxisRaw("Vertical");
-        Vector3 moveBy = transform.right * x + transform.forward * z;
-        rb.MovePosition(transform.position + moveBy.normalized * speed * Time.deltaTime);
+
+    void FixedUpdate()
+    {
+        Move();
+        Rotate();
+        JumpMove();
+        UpdateUI();
+        if(rb.position.y <= 0.6f)
+            currentState = CubeState.Idle;
+        else
+            currentState = CubeState.Jumping;
     }
 
-    bool isInState(CubeState state) {
-        return currentState == state;
+    void Move ()
+    {
+        if(currentState != CubeState.Jumping) {
+            float hAxis = Input.GetAxisRaw("Horizontal");
+            float vAxis = Input.GetAxisRaw("Vertical");
+
+            if(hAxis != 0 || vAxis != 0 && currentState != CubeState.Jumping)
+                currentState = CubeState.Moving;
+
+            Vector3 movement = new Vector3(hAxis, 0f, vAxis);
+            rb.position += movement * moveSpeed * Time.deltaTime;   
+        }
+    }
+
+    // Rotate the cube left if q is pressed and right when e is pressed
+    void Rotate() {
+        if(Input.GetKey(KeyCode.Q) || Input.GetKey(KeyCode.E)) {
+            if(Input.GetKey(KeyCode.Q))
+                transform.Rotate(new Vector3(rotationSpeed, 0f, 0f));
+            else if(Input.GetKey(KeyCode.E))
+                transform.Rotate(new Vector3(-rotationSpeed, 0f, 0f));
+            currentState = CubeState.Rotating;
+        }
+    }
+
+    // check if the player is on the ground and moving, when space is pressed jump
+    void JumpMove() {
+        if (currentState == CubeState.Moving && Input.GetKey(KeyCode.Space) && rb.velocity.y <= 0f) {
+            rb.AddForce(Vector3.up * jumpHeight, ForceMode.Impulse);
+            currentState = CubeState.Jumping;
+        }
+    }
+
+    void UpdateUI() {
+        switch(currentState) {
+            case CubeState.Idle:
+                GetComponent<Renderer>().material.color = Color.white;
+                break;
+            case CubeState.Moving:
+                GetComponent<Renderer>().material.color = Color.green;
+                break;
+            case CubeState.Rotating:
+                GetComponent<Renderer>().material.color = Color.black;
+                break;
+            case CubeState.Jumping:
+                GetComponent<Renderer>().material.color = Color.blue;
+                break;
+        }
     }
 }
 
