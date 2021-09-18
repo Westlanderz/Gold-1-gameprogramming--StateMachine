81571d744e65024af82eab48db27983984347dd8
Noah Knegt <github@noahknegt.com>
2021-09-18 13:00:08 +0200

    The state changes will smoothen
    Signed-off-by: Noah Knegt <github@noahknegt.com>

--- Assets/Scripts/CubeStates.cs
+++ Assets/Scripts/CubeStates.cs
@@ -36,21 +36,24 @@ public class MovingState : IStates {
         float hAxis = Input.GetAxisRaw("Horizontal");
         float vAxis = Input.GetAxisRaw("Vertical");
 
-        Vector3 movement = new Vector3(hAxis, 0f, vAxis);
-        cube.rb.position += movement * moveSpeed * Time.deltaTime;   
-
-        if (Input.GetKey(KeyCode.Space) && cube.rb.velocity.y <= 0f) {
-            JumpingState state = new JumpingState();
-            state.SetContext(cube);
-            cube.UpdateState(state);
-        } else if(Input.GetKey(KeyCode.Q) || Input.GetKey(KeyCode.E)) {
-            RotatingState state = new RotatingState();
-            state.SetContext(cube);
-            cube.UpdateState(state);
-        } else if(hAxis == 0 || vAxis == 0 && cube.rb.velocity.z <= 0f && cube.rb.velocity.x <= 0f) {
-            IdleState state = new IdleState();
-            state.SetContext(cube);
-            cube.UpdateState(state);
+        if(hAxis != 0 || vAxis != 0) {
+            Vector3 movement = new Vector3(hAxis, 0f, vAxis);
+            cube.rb.position += movement * moveSpeed * Time.deltaTime;   
+            if (Input.GetKey(KeyCode.Space) && cube.rb.velocity.y <= 0f) {
+                JumpingState state = new JumpingState();
+                state.SetContext(cube);
+                cube.UpdateState(state);
+            }
+        } else {
+            if(Input.GetKey(KeyCode.Q) || Input.GetKey(KeyCode.E)) {
+                RotatingState state = new RotatingState();
+                state.SetContext(cube);
+                cube.UpdateState(state);
+            } else if(hAxis == 0 || vAxis == 0 && cube.rb.velocity.z == 0f && cube.rb.velocity.x == 0f) {
+                IdleState state = new IdleState();
+                state.SetContext(cube);
+                cube.UpdateState(state);
+            }
         }
     }
 
@@ -109,7 +112,7 @@ public class JumpingState : IStates {
             cube.rb.AddForce(Vector3.up * jumpHeight, ForceMode.Impulse);
             jumped = true;
         }
-        if(cube.rb.position.y <= 0.6f && cube.rb.velocity.y == 0f) {
+        if(cube.rb.velocity.y == 0f && cube.rb.velocity.z == 0f && cube.rb.velocity.x == 0f) {
             IdleState state = new IdleState();
             state.SetContext(cube);
             cube.UpdateState(state);
